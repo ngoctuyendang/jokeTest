9e9d3af6f584d3d6153be499a2badfdc57a1ae71
ngoctuyendang <hanphan97nt@gmail.com>
2023-11-10 00:26:40 +0700

    Update solution of algorithm

--- app/src/main/java/com/example/dailyjoke/Algorithm.kt
+++ app/src/main/java/com/example/dailyjoke/Algorithm.kt
@@ -8,20 +8,10 @@ fun findMinMax(data: IntArray) {
     val size = data.size
     data.sort()
     val sum = data.sum()
-    when (size) {
-        0 -> {
-            println("Sum min: 0, Sum max: 0")
-        }
-        1 -> {
-            println("Sum: $sum")
-        }
-        else -> {
-            val sumMin = sum - data[size - 1]
-            val sumMax = sum - data[0]
-            println(
-                "Sum: $sum, Sum min: $sumMin, Sum max: $sumMax, " +
-                        "Min_value: ${data[0]}, Max_value: ${data[size - 1]}"
-            )
-        }
-    }
+    val sumMin = sum - data[size - 1]
+    val sumMax = sum - data[0]
+    println(
+        "Sum: $sum, Sum min: $sumMin, Sum max: $sumMax, " +
+                "Min_value: ${data[0]}, Max_value: ${data[size - 1]}"
+    )
 }
